faf3e74ff29c4481bab3b420750b35a316d5c6ad
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-06-05 13:06:33 -0600

    ✅ [ADD] Se copio la carpeta assets
    Ahora la carpeta assets se copio en el build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { type } = require('os');
 
 module.exports = {
   entry: './src/index.js',
@@ -26,6 +28,10 @@ module.exports = {
         test: /\.styl$/i,
         use: ['style-loader', 'css-loader', 'stylus-loader'],
       },
+      {
+        test: /\.(png|gif|jpg)$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
@@ -35,5 +41,13 @@ module.exports = {
       filename: './index.html',
     }),
     new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images',
+        },
+      ],
+    }),
   ],
 };
