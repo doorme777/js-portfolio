9426a3dee1aa54164567e063bd679a826f53de77
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-06-05 12:26:07 -0600

    ✅ [ADD] Se agrego el plugin mini css.
    Ahora ya esta se bundlea el css, cómo el preprocesador de staylus.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,6 +18,14 @@ module.exports = {
         exclude: /node_modules/,
         use: { loader: 'babel-loader' },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.styl$/i,
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
+      },
     ],
   },
   plugins: [
@@ -25,5 +34,6 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
