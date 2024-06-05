33a07a16e4bdcafbef7bc4a35002fb607d10c5d3
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-06-05 13:08:11 -0600

    ✅ [ADD] Se optimizaron las imagenes
    Ahora las imagenes son más ligeras.

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'stylus-loader'],
       },
       {
-        test: /\.(png|gif|jpg)$/,
+        test: /\.png$/,
         type: 'asset/resource',
       },
     ],
