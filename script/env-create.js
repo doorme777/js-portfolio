9c9c6624275142b9fbc42889e8e707907450fd31
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-06-07 09:49:48 -0600

    🐛 [FIX] Ahora ya función el build con script.

--- script/env-create.js
+++ script/env-create.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 
-fs.writeFile('./.env', `API_KEY=${process.env.API_KEY}\n`);
+fs.writeFile('./.env', `API_KEY=${process.env.API_KEY}\n`, 'utf8', (err) => {
+  if (err) {
+    console.error('Error al escribir el archivo:', err);
+  } else {
+    console.log('Archivo .env creado con éxito');
+  }
+});
